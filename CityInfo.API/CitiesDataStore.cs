f384c093fa3b8fe88d4e9f430281ffe6fa5724fe
Bartosz Wajsowicz <b.wajsowicz@gmail.com>
2018-03-24 22:37:12 +0100

    +CityDTO, CityDataStore, CityController.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,40 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDTO> Cities { get; set; }
+        
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDTO>
+            {
+                new CityDTO()
+                {
+                    Id = 1,
+                    Name = "Warsaw",
+                    Description = "Capital of Poland."
+                },
+                new CityDTO()
+                {
+                    Id = 2,
+                    Name = "Berlin",
+                    Description = "Capital of Germany."
+                },
+                new CityDTO()
+                {
+                    Id = 3,
+                    Name = "Praga",
+                    Description = "Capital of Czech Republic."
+                },
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CityController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CityController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+            => new JsonResult(CitiesDataStore.Current.Cities);
+
+        [HttpGet("{id}")]
+        public JsonResult GetSingleCity(int id)
+            => new JsonResult(CitiesDataStore.Current.Cities.SingleOrDefault(x => x.Id == id));
+    }
+}
